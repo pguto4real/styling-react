1cff5c0575bc75b824e7882cd6bb6d055f9a9c75
oluwamuibiclear <igbokweudoka@gmail.com>
2025-01-12 17:54:09 -0600

    header tailwind

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,17 @@
-import logo from '../assets/logo.png';
+import logo from "../assets/logo.png";
 
 export default function Header() {
   return (
-    <header>
-      <img src={logo} alt="A canvas" />
-      <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+    <header className="mb-8 md:mb-16 mt-8 flex flex-col items-center">
+      <img
+        src={logo}
+        alt="A canvas"
+        className="mb-8 h-44 w-44 object-contain"
+      />
+      <h1 className="text-center font-title text-xl md:text-4xl font-semibold uppercase tracking-widest text-amber-800">
+        ReactArt
+      </h1>
+      <p className="text-stone-500">A community of artists and art-lovers.</p>
     </header>
   );
 }
